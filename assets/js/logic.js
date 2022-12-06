426d51a0965a73c72b9c789c83519f55a55e9175
DOX7 <115791497+DarrenOxley7@users.noreply.github.com>
2022-12-06 22:18:43 +0000

    updates logic.js and changes questions objects-still does not work

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,18 @@
 //Global Variables
-// sets time to complete the quiz - 15 secs per question
-var timeRemaining = questions.length * 15;
+// sets time to complete the quiz - 10 secs per question
+var timeRemaining = questions.length * 10;
 // sets timer element on landing screen
 var timerEl = document.getElementById("time");
 var time = timeRemaining;
 var questionsIndex = 0;
-var Score = 0;
+var score = 0;
 var userChoiceSelected = false;
 // stores start button reference as variable
 var startBtn = document.getElementById("start");
 // refers to <a> tag that links to high scores
 var allAnchors = document.querySelector("a");
 //Global Variables End
+
 
 //adds question to page
 function showQuestion () {
@@ -34,6 +35,32 @@ function createChoices(questionObject) {
 }
 
 //add feedback
+function givesFeedback(correctChoice) {
+    // references feedback HTML element
+    var feedbackEl = document.getElementById("feedback");
+    var noAudioElementFound = document.getElementById("feedback-audio") === null;
+    if (noAudioElementFound) {
+        var audioElement = document.createElement("audio");
+        console.log(audioElement);
+        audioElement.setAttribute("id", "feedback-audio");
+    } else {
+        var audioElement = document.getElementById("feedback-audio");
+    }
+    if (correctChoice) {
+        feedbackEl.innerText = "Correct!";
+        console.log(document.getElementById("feedback-audio"));
+        audioElement.setAttribute("src", "assets/sfx/correct.wav");
+        audioElement.play();
+    } else {
+        feedbackEl.innerText = "Incorrect!";
+        audioElement.setAttribute("src", "assets/sfx/incorrect.wav");
+        audioElement.play();
+    }
+    feedbackEl.classList.remove("hide");
+    setTimeout(function () {
+        feedbackEl.classList.add("hide");
+    }, 9000);
+}
 
 
 //Start button
@@ -46,12 +73,12 @@ startBtn.addEventListener ("click", function() {
     questionsEl.classList.remove("hide");
 
     var timer = setInterval(function (){
-        var timerEl =document.getElementById("time")
+        var timerEl = document.getElementById("time")
         timerEl.innerHTML = time - 1;
         time --;
 
         //stops timer if time runs out
-        if (time < 1) {
+        if (time <= 0) {
             clearTimeout(timer);
             questionsEl.classList.add("hide")
 
@@ -60,14 +87,61 @@ startBtn.addEventListener ("click", function() {
             timerEl.innerHTML = 0;
 
             var finalScoreEl = document.getElementById("final-score")
-                if (userChoiceSelected = true) {
-                    finalScoreEl.textContent = "0"
-                    Score= 0;
+                if (userChoiceSelected) {
+                    finalScoreEl.textContent = score;
+                }
+                else {
+                    finalScoreEl.textContent = "0";
+                    score = 0;
                 }
         }
     }, 1000);
+    //shows the first question
     showQuestion()
 });
 
 
 
+var choicesEl = document.getElementById("choices");
+choicesEl, addEventListener ("click", function(event){
+var questionObject = questions[questionsIndex];
+var buttonClicked = (event.target.tagName.toUpperCase() === "button") &&
+event.path[1].className == "choices";
+var correctChoice = event.target.value === questionObject.answer;
+var isLastQuestion = questionsIndex === questions.length - 1;
+if (isLastQuestion && buttonClicked && correctChoice) {
+    userChoiceSelected = true;
+    choicesEl.innerHTML = '';
+    score = time;
+    givesFeedback(correctChoice)
+    time = 0;
+}
+else if (isLastQuestion && buttonClicked && !correctChoice) {
+    userChoiceSelected = true;
+    choicesEl.innerHTML = ``;
+    time -= 10;
+    score = time;
+    givesFeedback(correctChoice)
+    time = 0;
+}
+else if (buttonClicked && correctChoice) {
+    userChoiceSelected = true;
+    choicesEl.innerHTML = ``;
+    score = time;
+    givesFeedback(correctChoice)
+    questionsIndex++;
+    showQuestion()
+}
+else if(buttonClicked && !correctChoice) {
+    userChoiceSelected = true;
+    time -= 10;
+    choicesEl.innerHTML = ``;
+    score = time;
+    givesFeedback(correctChoice)
+    questionsIndex++;
+    showQuestion();
+}
+if (score < 0) {
+    score = 0;
+}
+})
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,26 +1,26 @@
 var questions = [ 
 
     {
-        title: 'Inside which element do you place the JavaScript',
+        question: 'Inside which element do you place the JavaScript',
         choices: ['<js>', '<javaScript>', '<script>', '<java>'],
-        correctAnswer: '<script>'
+        answer: '<script>'
     },
     
     {
-        title: 'How can you add a comment in a JavaScript?',
+        question: 'How can you add a comment in a JavaScript?',
         choices: ['<!--I am a comment>', '--I am a comment', '/I am a comment', '//I am a comment'],
-        correctAnswer: '//I am a comment'
+        answer: '//I am a comment'
     },
 
     {
-        title: 'How does a FOR loop start',
+        question: 'How does a FOR loop start',
         choices: ['for (i=0; i<=10; i++)', 'for i = 1-10', 'for (i=0; i<=10)', 'for (i<=10; i++)'],
-        correctAnswer: 'for (i=0; i<=10; i++)'
+        answer: 'for (i=0; i<=10; i++)'
     },
 
     {
-        title: 'How do you call a function named exampleFunction',
+        question: 'How do you call a function named exampleFunction',
         choices: ['call exampleFunction', 'exampleFunction()', 'call function exampleFunction()', 'call(exampleFunction)'],
-        correctAnswer: 'exampleFunction()'
+        answer: 'exampleFunction()'
     }
 ]
